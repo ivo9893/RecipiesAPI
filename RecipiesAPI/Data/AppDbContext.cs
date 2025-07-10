45e1ea98940ef72ebafeaa25c70030bc3991e893
Ivko <ivo_stoqnov@mail.bg>
2025-07-10 19:51:07 +0300

    models created

---
+++ RecipiesAPI/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipiesAPI.Models;
+
+namespace RecipiesAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Image> Images { get; set; }
+    }
+}
---
+++ RecipiesAPI/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipiesAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)] // Assuming a reasonable max length for names
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(255)] // Assuming a reasonable max length for email
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8)] // Assuming a minimum password length
+        [MaxLength(255)] // Assuming a reasonable max length for hashed password
+        public string Password { get; set; } // In a real application, this would be a hashed password
+
+        // Navigation property for Recipes authored by this user
+        public ICollection<Recipe> Recipes { get; set; }
+    }
+}
