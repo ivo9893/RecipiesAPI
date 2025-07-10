329a269254bc7c224a814b8383842e4c8924b60f
Ivko <ivo_stoqnov@mail.bg>
2025-07-10 21:03:52 +0300

    added create user endpoint

---
+++ RecipiesAPI/Migrations/20250710180219_AddChanges.cs
@@ -0,0 +1,291 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipiesAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Recipe_RecipeId",
+                table: "Images");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipe_User_AuthorId",
+                table: "Recipe");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RecipeCategory_Categories_CategoryId",
+                table: "RecipeCategory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RecipeCategory_Recipe_RecipeId",
+                table: "RecipeCategory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RecipeIngredient_Recipe_RecipeId",
+                table: "RecipeIngredient");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RecipeIngredient",
+                table: "RecipeIngredient");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RecipeCategory",
+                table: "RecipeCategory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RecipeCategory_RecipeId",
+                table: "RecipeCategory");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Recipe",
+                table: "Recipe");
+
+            migrationBuilder.RenameTable(
+                name: "User",
+                newName: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "RecipeIngredient",
+                newName: "RecipeIngredients");
+
+            migrationBuilder.RenameTable(
+                name: "RecipeCategory",
+                newName: "RecipeCategories");
+
+            migrationBuilder.RenameTable(
+                name: "Recipe",
+                newName: "Recipes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RecipeIngredient_RecipeId",
+                table: "RecipeIngredients",
+                newName: "IX_RecipeIngredients_RecipeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RecipeCategory_CategoryId",
+                table: "RecipeCategories",
+                newName: "IX_RecipeCategories_CategoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Recipe_AuthorId",
+                table: "Recipes",
+                newName: "IX_Recipes_AuthorId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RecipeIngredients",
+                table: "RecipeIngredients",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RecipeCategories",
+                table: "RecipeCategories",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Recipes",
+                table: "Recipes",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeCategories_RecipeId_CategoryId",
+                table: "RecipeCategories",
+                columns: new[] { "RecipeId", "CategoryId" },
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Recipes_RecipeId",
+                table: "Images",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RecipeCategories_Categories_CategoryId",
+                table: "RecipeCategories",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RecipeCategories_Recipes_RecipeId",
+                table: "RecipeCategories",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RecipeIngredients_Recipes_RecipeId",
+                table: "RecipeIngredients",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_Users_AuthorId",
+                table: "Recipes",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Recipes_RecipeId",
+                table: "Images");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RecipeCategories_Categories_CategoryId",
+                table: "RecipeCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RecipeCategories_Recipes_RecipeId",
+                table: "RecipeCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RecipeIngredients_Recipes_RecipeId",
+                table: "RecipeIngredients");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_Users_AuthorId",
+                table: "Recipes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Recipes",
+                table: "Recipes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RecipeIngredients",
+                table: "RecipeIngredients");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RecipeCategories",
+                table: "RecipeCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RecipeCategories_RecipeId_CategoryId",
+                table: "RecipeCategories");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "User");
+
+            migrationBuilder.RenameTable(
+                name: "Recipes",
+                newName: "Recipe");
+
+            migrationBuilder.RenameTable(
+                name: "RecipeIngredients",
+                newName: "RecipeIngredient");
+
+            migrationBuilder.RenameTable(
+                name: "RecipeCategories",
+                newName: "RecipeCategory");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Recipes_AuthorId",
+                table: "Recipe",
+                newName: "IX_Recipe_AuthorId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RecipeIngredients_RecipeId",
+                table: "RecipeIngredient",
+                newName: "IX_RecipeIngredient_RecipeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RecipeCategories_CategoryId",
+                table: "RecipeCategory",
+                newName: "IX_RecipeCategory_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Recipe",
+                table: "Recipe",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RecipeIngredient",
+                table: "RecipeIngredient",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RecipeCategory",
+                table: "RecipeCategory",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeCategory_RecipeId",
+                table: "RecipeCategory",
+                column: "RecipeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Recipe_RecipeId",
+                table: "Images",
+                column: "RecipeId",
+                principalTable: "Recipe",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipe_User_AuthorId",
+                table: "Recipe",
+                column: "AuthorId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RecipeCategory_Categories_CategoryId",
+                table: "RecipeCategory",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RecipeCategory_Recipe_RecipeId",
+                table: "RecipeCategory",
+                column: "RecipeId",
+                principalTable: "Recipe",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RecipeIngredient_Recipe_RecipeId",
+                table: "RecipeIngredient",
+                column: "RecipeId",
+                principalTable: "Recipe",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ RecipiesAPI/Services/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using RecipiesAPI.Models.DTO;
+using RecipiesAPI.Models;
+
+namespace RecipiesAPI.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<User> CreateUserAsync(CreateUserDto userDto);
+    }
+}
