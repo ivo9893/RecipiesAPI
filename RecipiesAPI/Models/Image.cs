21ee21da1dc7770cffe0bafc0dee9f32cd00da43
Ivko <ivo_stoqnov@mail.bg>
2025-07-10 19:51:07 +0300

    models created

---
+++ RecipiesAPI/Models/Image.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecipiesAPI.Models
+{
+    public class Image
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Recipe")]
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; } // Navigation property
+
+        [Required]
+        [Url]
+        [MaxLength(2048)] // Standard max length for URLs
+        public string Url { get; set; } // URL to the image
+    }
+}
